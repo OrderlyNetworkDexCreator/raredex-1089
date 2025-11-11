f1c9dabde7c6cdf04e1db306690b3fd9748ca18e
Mario Reder <mario.reder@pm.me>
2025-11-11 22:28:57 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "Raredex",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/raredex-1089/",
+  "VITE_SEO_SITE_URL": "https://raredex.trade",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@Raredextrade",
